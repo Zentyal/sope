2e0b9183413d013db5ac776eaf3bae98e0576422
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2016-01-28 11:40:01 +0100

    Ensure NGSocket close method is called (avoid compilation warnings)

--- sope-core/NGStreams/NGActiveSocket.m
+++ sope-core/NGStreams/NGActiveSocket.m
@@ -186,7 +186,7 @@
         in which we could check the -lastException ...
       */
       e = [[sock lastException] retain];
-      [sock close];
+      [(NGSocket *)sock close];
       [sock release];
       [self release];
       e = [e autorelease];
